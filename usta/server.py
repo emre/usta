63f1e7524a387534a4fe698f5540ad761ed77bbb
Emre Yilmaz <mail@emreyilmaz.me>
2014-09-23 13:13:03 +0300

    added control for bad requests.

--- usta/server.py
+++ usta/server.py
@@ -5,7 +5,7 @@ import os
 import os.path
 
 
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from werkzeug import secure_filename
 
 
@@ -46,15 +46,22 @@ def main():
             return '.' in filename and \
                    filename.rsplit('.', 1)[1] in allowed_extensions
 
+        # suppose all file formats are okay to upload
+        # if the config doesn't exists a ALLOWED_EXTENSIONS section.
         return True
 
     @app.route('/upload', methods=['POST'])
     def upload():
-        _file = request.files['file']
-        if _file and allowed_file(_file.filename):
-            filename = secure_filename(_file.filename)
-            _file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return '', 201
+        _file = request.files.get("file")
+        if not _file:
+            return jsonify({"message": "a file named as 'file' required."}), 400
+
+        if not allowed_file(_file.filename):
+            return jsonify({"message": "invalid file type."}), 400
+
+        filename = secure_filename(_file.filename)
+        _file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return '', 201
 
     app.debug = True
     app.run(
